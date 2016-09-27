fae65b31269528a57ee8462566833cbeb8c5cd3c
dxl <duanxilun@duanxilundeMacBook-Air.local>
2016-09-27 09:33:31 +0900

    UITableVIew Demo study

---
+++ UITableViewDemo_0927/UITableViewDemo_0927/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  UITableViewDemo_0927
+//
+//  Created by dxl on 16/9/27.
+//  Copyright © 2016年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    UITableView *_myTableView;
+}
+
+@end
+
---
+++ UITableViewDemo_0927/UITableViewDemo_0927/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UITableViewDemo_0927
+//
+//  Created by dxl on 16/9/27.
+//  Copyright © 2016年 dxl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"TableViewDemo";
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
