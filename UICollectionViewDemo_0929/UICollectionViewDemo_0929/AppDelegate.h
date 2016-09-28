6766fdc95436e55ac703742e0fa547c71d3f2133
dxl <duanxilun@duanxilundeMacBook-Air.local>
2016-09-29 08:33:27 +0900

    create UICollectionViewDemo project

---
+++ UICollectionViewDemo_0929/UICollectionViewDemo_0929/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewDemo_0929
+//
+//  Created by dxl on 16/9/29.
+//  Copyright © 2016年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewDemo_0929/UICollectionViewDemo_0929/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionViewDemo_0929
+//
+//  Created by dxl on 16/9/29.
+//  Copyright © 2016年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
