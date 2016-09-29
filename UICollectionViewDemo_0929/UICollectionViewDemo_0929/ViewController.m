377e4f1354e0035fd382a47243bfb9901ca35a45
dxl <duanxilun@duanxilundeMacBook-Air.local>
2016-09-29 16:57:43 +0900

    add header and footer

--- UICollectionViewDemo_0929/UICollectionViewDemo_0929/ViewController.m
+++ UICollectionViewDemo_0929/UICollectionViewDemo_0929/ViewController.m
@@ -37,11 +37,18 @@
 {
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
     layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+    layout.minimumLineSpacing = 2;
+    layout.minimumInteritemSpacing = 5;
     self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:layout];
     self.collectionView.showsVerticalScrollIndicator = NO;
     self.collectionView.delegate = self;
     self.collectionView.dataSource = self;
+    //register for cell
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"test"];
+    //register for header
+    [self.collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"headerID"];
+    //register for footer
+    [self.collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"footerID"];
     self.collectionView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.collectionView];
     
@@ -54,6 +61,7 @@
     CGFloat pointX = (CellWidth + 10) * columnIndex;
     NSLog(@"%f",pointX);
     CGFloat newPointY = 10;
+    
     for (NSUInteger i = indexPath.row - Column; i > 0; i-=Column) {
         newPointY += [self.cellsHeight[i] floatValue]+ 10;
     }
@@ -69,7 +77,7 @@
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return 10;
+    return 12;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
@@ -83,13 +91,75 @@
 //        cell.frame = CGRectMake(0, 0, CellWidth, [self.cellsHeight[indexPath.row] floatValue]);
 //    }
     
+    
+    
+    
     return cell;
+}
+
+//create header and footer
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionReusableView *reusableView;
+    if (kind == UICollectionElementKindSectionHeader) {
+        reusableView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"headerID"forIndexPath:indexPath];
+        reusableView.layer.borderWidth = 1;
+        reusableView.layer.borderColor = [UIColor grayColor].CGColor;
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [btn setTitle:@"Header" forState:UIControlStateNormal];
+        btn.frame = CGRectMake(0, 0, 320, 80);
+        [reusableView addSubview:btn];
+    }
+    if (kind == UICollectionElementKindSectionFooter) {
+        reusableView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"footerID"forIndexPath:indexPath];
+        
+        reusableView.layer.borderWidth = 1;
+        reusableView.layer.borderColor = [UIColor grayColor].CGColor;
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [btn setTitle:@"Footer" forState:UIControlStateNormal];
+        btn.frame = CGRectMake(0, 0, 320, 80);
+        NSLog(@"fuck");
+        [reusableView addSubview:btn];
+    }
+    
+    return reusableView;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section
+{
+    return CGSizeMake(320, 80);
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForFooterInSection:(NSInteger)section
+{
+    return CGSizeMake(320, 80);
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
     UICollectionViewCell *cell = [collectionView cellForItemAtIndexPath:indexPath];
     cell.backgroundColor = [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1];
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender
+{
+//    if ([NSStringFromSelector(action) isEqualToString:@"cut:"] || [NSStringFromSelector(action) isEqualToString:@"paste:"]) {
+//        return YES;
+//    }
+    return YES;
+}
+
+//failed: will crash when select
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender
+{
+    if ([NSStringFromSelector(action) isEqualToString:@"cut:"] ) {
+        [collectionView reloadData];
+    }
 }
 
 #pragma mark - UICollectionViewDelegateFlowLayout
@@ -103,7 +173,7 @@
 
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
 {
-    return UIEdgeInsetsMake(0, 0, 0, 0);
+    return UIEdgeInsetsMake(10, 0, 10, 0);
 }
 
 @end
